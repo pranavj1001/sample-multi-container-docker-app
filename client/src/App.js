30158b4fd093a61e671228e273027136d0a968dd
Pranav Jain <pranavj1001@gmail.com>
2019-05-12 20:29:25 +0530

    setup routes for the client side app

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import HomePage from './HomePage';
+import FibonacciCalculator from './FibonacciCalculator';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <Link to="/">Calculator</Link>
+          <Link to="/home">Home</Link>
+        </header>
+        <div>
+          <Route exact path="/" component={FibonacciCalculator} />
+          <Route exact path="/home" component={HomePage} />
+        </div>
+      </div>
+    </Router>
   );
 }
 
